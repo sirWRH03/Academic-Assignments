4971d5d0b4719139d7e49b8e5c219c41b4982249
Will Humphrey <61913098+sirWRH03@users.noreply.github.com>
2024-11-25 21:37:43 -0600

    Update ProgAss3.pl

--- CS424/ProgAss3.pl
+++ CS424/ProgAss3.pl
@@ -1,3 +1,13 @@
+% Will Humphrey
+% 11/25/2024
+% I did not copy my answers from another person, nor did I use an AI tool to help me complete this assignment.
+
+% Part 1
+countValues(_, [], 0).
+countValues(X, [X | T], Z) :- countValues(X, T, Z2), Z is Z2 + 1.
+countValues(X, [_ | T], Z) :- countValues(X, T, Z).
+
+% Part 2
 male(will).
 male(matt).
 male(billy).
